6c265d5a72227c4ad04293113363e4b2da0dea02
Matina Patsos <matinaspatsos@gmail.com>
2019-04-16 21:55:02 -0400

    Redux thunk exercise

---
+++ react-exercises/22-redux-thunk/src/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { render } from "react-dom";
+import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
+import store from "./store";
+import App from "./components/App/App";
+import "./index.css";
+
+render(
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>,
+  document.getElementById("root")
+);
