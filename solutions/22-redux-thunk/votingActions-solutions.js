deadf98fe58c9e72ee68ccee87a28ba52c10b88e
Matina Patsos <matinaspatsos@gmail.com>
2019-04-27 20:51:00 -0400

    Redux thunk solutions

---
+++ solutions/22-redux-thunk/votingActions-solutions.js
@@ -0,0 +1,47 @@
+import * as types from "../constants/actionTypes";
+
+import { voteRequest } from "../services/api";
+
+/**
+ * Using "src/actions/fetchAllNomineesActions.js" as an example, setup action creators for:
+ * - loading ajax request
+ * - ajax request failure
+ */
+
+const ajaxLoading = () => {
+  return {
+    type: types.VOTE_PENDING
+  };
+};
+
+const ajaxSuccess = (categoryId, nomineeIndex) => {
+  return {
+    type: types.VOTE_FULFILLED,
+    categoryId,
+    nomineeIndex
+  };
+};
+
+const ajaxRejected = () => {
+  return {
+    type: types.VOTE_REJECTED
+  };
+};
+
+export const vote = (categoryId, nomineeIndex) => {
+  return dispatch => {
+    /**
+     * Using "src/actions/fetchAllNomineesActions.js" as an example, do the following:
+     * - dispatch the loading ajax action
+     * - call on and pass the necesarry arguements to the voteRequest function
+     * (see "src/services/api.js")
+     * - dispatch the ajaxSuccess action when the ajax request is successfully,
+     * passing all the necessary arguments
+     * - dispatch the ajax request failure action when an error is caught
+     */
+    dispatch(ajaxLoading());
+    voteRequest(categoryId, nomineeIndex)
+      .then(() => dispatch(ajaxSuccess(categoryId, nomineeIndex)))
+      .catch(() => dispatch(ajaxRejected()));
+  };
+};
